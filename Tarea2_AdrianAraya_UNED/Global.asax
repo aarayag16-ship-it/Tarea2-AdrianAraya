2aa7cf09293dcc0653da3bc46f41e6dd8cd51403
Adrian Araya <aarayag16@gmail.com>
2025-10-04 18:45:17 -0600

    commit de prueba

---
+++ Tarea2_AdrianAraya_UNED/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea2_AdrianAraya_UNED.MvcApplication" Language="C#" %>
---
+++ Tarea2_AdrianAraya_UNED/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarea2_AdrianAraya_UNED.Startup))]
+namespace Tarea2_AdrianAraya_UNED
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
